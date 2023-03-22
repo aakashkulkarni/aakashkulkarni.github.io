b86a1cdcfe808679eef46eff9e5ad5f8290b7980
aakashkulkarni <aakash.utah@gmail.com>
2023-03-21 23:33:49 -0600

    redesigned pages and made website responsive

--- main.js
+++ main.js
@@ -18,3 +18,33 @@ function makeActive(buttonName) {
     // Add active class to the selected tab
     document.getElementById(buttonName).classList.add("active");
 }
+
+function openTab2(tabName, buttonName) {
+  makeActive2(buttonName);
+  var i;
+  var x = document.getElementsByClassName("tab-item");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  document.getElementById(tabName).style.display = "block";  
+}
+
+function makeActive2(buttonName) {
+  // Remove active class from any other active tabs
+  var x = document.getElementsByClassName("btn2")
+  for (i = 0; i < x.length; i++) {
+      x[i].classList.remove("active1")
+  }
+
+  // Add active class to the selected tab
+  document.getElementById(buttonName).classList.add("active1");
+}
+
+function hamburger() {
+  var x = document.getElementById("hidden-nav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
